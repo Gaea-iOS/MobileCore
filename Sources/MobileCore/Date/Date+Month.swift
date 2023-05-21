b12484e13b71f0e39be8a445bf009cafc84ef3e6
Jerry Wang <445242970@qq.com>
2023-05-21 22:45:45 +0800

    Update Date+Month

--- Sources/MobileCore/Date/Date+Month.swift
+++ Sources/MobileCore/Date/Date+Month.swift
@@ -51,3 +51,41 @@ public extension Date {
         return previousMonthDate.monthDates(in: calendar)
     }
 }
+
+public extension Date {
+    func monthDatesGroupInWeek(in calendar: Calendar = .current) -> [[Date]] {
+        let startOfMonth = startOfMonth(in: calendar)
+        let endOfMonth = endOfMonth(in: calendar)
+        
+        let startWeekday = startOfMonth.weekday(in: calendar)
+        let prefixDates = (1..<startWeekday)
+            .map {
+                calendar.date(byAdding: .day, value: -(startWeekday - $0), to: startOfMonth)!
+            }
+        
+        let endWeekday = endOfMonth.weekday(in: calendar)
+        let postfixDates = ((endWeekday + 1)...7)
+            .map {
+                calendar.date(byAdding: .day, value: $0 - endWeekday, to: endOfMonth)!
+            }
+
+        let dates = prefixDates + monthDates(in: calendar) + postfixDates
+        precondition(dates.count % 7 == 0)
+
+        let numberOfGroup = dates.count / 7
+        
+        return (0..<numberOfGroup).map {
+            Array(dates[($0 * 7)...($0 * 7 + 6)])
+        }
+    }
+    
+    func previousMonthDatesGroupInWeek(in calendar: Calendar = .current) -> [[Date]] {
+        let previousMonthDate = calendar.date(byAdding: .month, value: -1, to: self)!
+        return previousMonthDate.monthDatesGroupInWeek(in: calendar)
+    }
+    
+    func nextMonthDatesGroupInWeek(in calendar: Calendar = .current) -> [[Date]] {
+        let previousMonthDate = calendar.date(byAdding: .month, value: 1, to: self)!
+        return previousMonthDate.monthDatesGroupInWeek(in: calendar)
+    }
+}
