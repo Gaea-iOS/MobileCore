981ff1d096cd1d62426593ce89cde1060285e556
Jerry Wang <445242970@qq.com>
2023-05-22 10:49:01 +0800

    Add GroupIn func

---
+++ Sources/MobileCore/Collection/Array+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jerrywang on 2023/5/21.
+//
+
+import Foundation
+
+public extension Array {
+    func group(in number: Int) -> [[Element]] {
+        let numberOfGroup = count / number
+        
+        var result = (0..<numberOfGroup).map {
+            let indexRange = ($0 * number)..<(($0 + 1) * number)
+            return Array(self[indexRange])
+        }
+        
+        if count & number != 0 {
+            let restIndexRange = (number * numberOfGroup)..<count
+            let rest = Array(self[restIndexRange])
+            result.append(rest)
+        }
+        
+        return result
+    }
+}
