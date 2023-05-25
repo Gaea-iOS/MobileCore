193a8c73ad7d0e17b1899d2dba0e750c22594fb0
Jerry Wang <445242970@qq.com>
2023-05-25 16:54:10 +0800

    Rename function

--- Sources/MobileCore/Date/Date+Display.swift
+++ Sources/MobileCore/Date/Date+Display.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 public extension Date {
-    func diplay(format: DateFormatter) -> String {
+    func display(format: DateFormatter) -> String {
         return format.string(from: self)
     }
 
-    func diplay(format: String) -> String {
+    func display(format: String) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
         return dateFormatter.string(from: self)
