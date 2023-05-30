db36bdd52fa866399b0df91e8310b220afd7e5e4
Jerry Wang <445242970@qq.com>
2023-05-30 17:22:08 +0800

    Fill Date+Compare

--- Sources/MobileCore/Date/Date+Compare.swift
+++ Sources/MobileCore/Date/Date+Compare.swift
@@ -2,21 +2,61 @@
 //  File.swift
 //  
 //
-//  Created by Jerrywang on 2023/5/11.
+//  Created by 王小涛 on 2023/5/30.
 //
 
 import Foundation
 
 public extension Date {
-    func isInSameDay(as date: Date, in calendar: Calendar = .current) -> Bool {
-        calendar.isDate(date, inSameDayAs: self)
+    private func isEqual(to date: Date, toGranularity component: Calendar.Component, in calendar: Calendar = .current) -> Bool {
+        calendar.isDate(self, equalTo: date, toGranularity: component)
+    }
+
+    func isInSameYear(as date: Date, in calendar: Calendar = .current) -> Bool {
+        isEqual(to: date, toGranularity: .year, in: calendar)
     }
     
     func isInSameMonth(as date: Date, in calendar: Calendar = .current) -> Bool {
-        calendar.isDate(date, equalTo: self, toGranularity: .month)
+        isEqual(to: date, toGranularity: .month, in: calendar)
     }
     
-    func isInSameYear(as date: Date, in calendar: Calendar = .current) -> Bool {
-        calendar.isDate(date, equalTo: self, toGranularity: .year)
+    func isInSameWeek(as date: Date, in calendar: Calendar = .current) -> Bool {
+        isEqual(to: date, toGranularity: .weekOfYear, in: calendar)
+    }
+
+    func isInSameDay(as date: Date, in calendar: Calendar = .current) -> Bool {
+        calendar.isDate(self, inSameDayAs: date)
+    }
+
+    func isInThisYear(in calendar: Calendar = .current) ->  Bool {
+        isInSameYear(as: Date())
+    }
+    
+    func isInThisMonth(in calendar: Calendar = .current) ->  Bool {
+        isInSameMonth(as: Date())
+    }
+    
+    func isInThisWeek(in calendar: Calendar = .current) ->  Bool {
+        isInSameWeek(as: Date())
+    }
+
+    func isInYesterday(in calendar: Calendar = .current) ->  Bool {
+        calendar.isDateInYesterday(self)
+    }
+    
+    func isInToday(in calendar: Calendar = .current) -> Bool {
+        calendar.isDateInToday(self)
+    }
+    
+    func isInTomorrow(in calendar: Calendar = .current) -> Bool {
+        calendar.isDateInTomorrow(self)
+    }
+
+    var isInTheFuture: Bool {
+        self > Date()
+    }
+    
+    var isInThePast:   Bool {
+        self < Date()
     }
 }
