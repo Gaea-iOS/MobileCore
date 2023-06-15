cc0481b9882d3e31ee9eaa5ff6de7914abaad3df
Jerry Wang <445242970@qq.com>
2023-06-15 12:02:36 +0800

    Public CodableSerializer

--- Sources/MobileCore/Serializing/CodableSerializer.swift
+++ Sources/MobileCore/Serializing/CodableSerializer.swift
@@ -7,14 +7,14 @@ import Foundation
 typealias Encode<T: Encodable> = (T) throws -> Data
 typealias Decode<T: Decodable> = (Data) throws -> T
 
-final class EncodableSerializer<Input>: Serializer where Input: Encodable {
+public final class EncodableSerializer<Input>: Serializer where Input: Encodable {
     private let encode: Encode<Input>
 
     init(encode: @escaping Encode<Input> = JSONEncoder().encode) {
         self.encode = encode
     }
 
-    func serialize(_ input: Input) throws -> Data {
+    public func serialize(_ input: Input) throws -> Data {
         try encode(input)
     }
 }
@@ -25,22 +25,22 @@ extension JSONDecoder {
     }
 }
 
-final class DecodableDeserializer<Output>: Deserializer where Output: Decodable {
+public final class DecodableDeserializer<Output>: Deserializer where Output: Decodable {
     private let decode: Decode<Output>
 
     init(decode: @escaping Decode<Output> = JSONDecoder().decode(from:)) {
         self.decode = decode
     }
 
-    func deserialize(_ data: Data) throws -> Output {
+    public func deserialize(_ data: Data) throws -> Output {
         try decode(data)
     }
 }
 
-func encodableSerializer<T: Encodable>(encoder: JSONEncoder = .init()) -> EncodableSerializer<T> {
+public func encodableSerializer<T: Encodable>(encoder: JSONEncoder = .init()) -> EncodableSerializer<T> {
     EncodableSerializer<T>(encode: encoder.encode(_:))
 }
 
-func decodableDeserializer<T: Decodable>(decoder: JSONDecoder = .init()) -> DecodableDeserializer<T> {
+public func decodableDeserializer<T: Decodable>(decoder: JSONDecoder = .init()) -> DecodableDeserializer<T> {
     DecodableDeserializer<T>(decode: decoder.decode(from:))
 }
